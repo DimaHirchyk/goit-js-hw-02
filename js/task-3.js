f2b3c943fc3152932c3fc96b562180a80705f347
Dima Hirchyk <dimzuk04@gmail.com>
2024-11-04 14:48:05 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,21 @@
-function getElementWidth(content, padding, border) {
-  const contentNum = Number.parseFloat(content);
-  const paddingNum = Number.parseFloat(padding);
-  const borderNum = Number.parseFloat(border);
+function checkForSpam(message) {
+  const smallMessage = message.toLowerCase();
 
-  return contentNum + paddingNum * 2 + borderNum * 2;
+  return (
+    smallMessage.includes("spam") ||
+    smallMessage.includes("sale")
+  );
 }
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(
+  checkForSpam("Trust me, this is not a spam message")
+); // true
+console.log(
+  checkForSpam(
+    "Get rid of sPaM emails. Our book in on sale!"
+  )
+); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
